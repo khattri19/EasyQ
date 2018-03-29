c427d8dc2049ad2ad1bbdcdb7201556022c1250c
Rajan Jha <jharajan20@gmail.com>
2018-03-30 01:31:06 +0530

    Added Web Version

---
+++ Easy_Web/index.php
@@ -0,0 +1,99 @@
+<!Doctype HTML>
+
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css?family=Pacifico|Bangers|Gloria+Hallelujah" rel="stylesheet"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script type="text/javascript" src="jquery-1.11.0.min.js"></script>
+<script type="text/javascript">
+
+$({
+   
+});
+
+ $(document).scroll(function()
+    {
+        if($(document).scrollTop() > 500)
+        {
+            $("#header").css("font-size","80%");
+            $("#logo").css("font-size","180%");
+        }
+        else if($(document).scrollTop() < 500)
+        {
+            $("#header").css("font-size","100%");
+            $("#logo").css("font-size","250%");
+        }
+    });
+
+</script>
+<link rel="stylesheet" href="index.css" />
+</head>
+
+<body>
+
+<div id="container">
+
+<div id="header">
+
+<div id="searchForm">
+<div class="input-group mb-3" style="width: 150%;position: relative;left: -45%;">
+  <input type="text" class="form-control" placeholder="Search Queue Here" aria-label="Recipient's username" aria-describedby="basic-addon2">
+  <div class="input-group-append">
+    <span class="input-group-text" id="basic-addon2"><img src="search.png" /></span>
+  </div>
+</div>
+<div id="customerForm">
+<table id="customerFormTable">
+<tr>
+<td><img src="login.png" /><br />Sign In</td>
+<td><img src="signup.png" /><br />Sign Up</td>
+<td><img src="location.png" /><br />Location: Allahabad</td>
+</tr>
+</table>
+</div>
+</div>
+
+</div>
+
+<div id="logo">EasyQ</div>
+
+<div id="dood1" class="dood">
+Now You Can Join Any Queue From Anywhere. No Worries.
+</div>
+<div id="dood2" class="dood">Search The Queue And Join It</div>
+<div id="dood3" class="dood"> Know Queue Time | Save Time</div>
+<div id="dood4" class="dood">Banks</div>
+<div id="dood5" class="dood">Hospitals</div>
+<div id="dood6" class="dood">Hotels</div>
+<div id="dood7" class="dood">Theatres</div>
+<div id="dood8" class="dood">Restaurants</div>
+<div id="dood9" class="dood">Shops</div>
+<div id="dood10" class="dood" style="text-decoration: line-through;">Anxiety</div>
+<div id="dood11" class="dood">HAPPINESS</div>
+<div id="dood12" class="dood">boredom</div>
+<div id="dood13" class="dood">TIME IS THE MOST VALUE COMODITY DON'T <line>WASTE</line> IT STANDING</div>
+<div id="dood14"><img src="queue.png" style="width: 65%;" /></div>
+<div id="dood15" class="dood">Institutes</div>
+<div id="dood16" class="dood">Simple</div>
+
+<div id="trending">
+<div id="heading" class="trendingItem" style="background: #007DA8;color: #FFF;"> #Trending</div>
+<div id="trendingItem1" class="trendingItem">Trend1</div>
+<div id="trendingItem2" class="trendingItem">Trend2</div>
+<div id="trendingItem3" class="trendingItem">Trend3</div>
+<div id="trendingItem4" class="trendingItem">Trend4</div>
+<div id="trendingItem5" class="trendingItem">Trend5</div>
+<div id="trendingItem6" class="trendingItem">Trend6</div>
+<div id="trendingItem7" class="trendingItem">Trend7</div>
+<div id="trendingItem8" class="trendingItem">Trend8</div>
+<div id="trendingItem9" class="trendingItem">Trend9</div>
+<div id="trendingItem10" class="trendingItem">Trend10</div>
+<div id="trendingItem12" class="trendingItem">Trend11</div>
+<div id="trendingItem13" class="trendingItem">Trend12</div>
+<div id="trendingItem14" class="trendingItem">Trend13</div>
+</div>
+
+</div>
+</body>
+
+</html>
